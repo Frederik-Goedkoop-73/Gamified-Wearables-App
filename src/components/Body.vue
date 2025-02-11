cafe15be2955ab37d903f168dbdb54f6f4a32f46
Frederik Goedkoop <frederik@goedkoop.biz>
2025-02-11 23:54:14 +0100

    initial commit

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -9,13 +9,7 @@ import { ref } from 'vue';
     <div class="body-box grey-border">
       <h2>Health stats</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident quasi, aliquid sint, illum quae ut repellendus numquam officia corrupti voluptate vitae consequuntur dolorem nostrum voluptates! Veritatis veniam quo aspernatur similique!
       </p>
       <img
         class="example-img"
@@ -27,13 +21,7 @@ import { ref } from 'vue';
     <div class="body-box grey-border">
       <h2>Quests</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum quisquam ipsa unde laboriosam et ratione quasi. Natus saepe aut quae cupiditate quaerat quos perferendis nam, atque quis error aperiam numquam.
       </p>
       <img
         class="example-img"
@@ -43,49 +31,19 @@ import { ref } from 'vue';
     </div>
 
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus iste expedita minus impedit nam doloremque similique laudantium, aut praesentium voluptates quae pariatur velit a et rem placeat voluptas, tempore unde.
     </p>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam minus consectetur quis odit vel iure explicabo quaerat iusto non vero iste laborum, neque sint molestias sapiente enim blanditiis accusantium culpa!
     </p>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel voluptatibus provident, adipisci eaque mollitia at laborum possimus odit assumenda sint, ea aperiam quisquam non magni quaerat? Ipsa iure iusto eaque?
     </p>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quibusdam vitae facilis animi aliquam deleniti esse mollitia, nesciunt unde ullam consequatur repudiandae hic molestiae doloribus cupiditate earum sequi. Tenetur, pariatur.
     </p>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat officia rerum voluptatibus quam quo ullam obcaecati inventore facilis et incidunt, quod minima facere laudantium doloremque molestiae natus qui aliquid soluta.
     </p>
   </main>
 </template>
